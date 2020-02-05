49fd7f9613e226f1b2cce5f6fa89c9aa5c9f0342
star909 <shivamsri30@gmail.com>
2020-02-05 13:51:33 +0530

    Implementing search functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from  '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { DataTableComponent } from './data-table/data-table.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
 
 @NgModule({
   declarations: [
@@ -12,7 +15,10 @@ import { DataTableComponent } from './data-table/data-table.component';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    Ng2SearchPipeModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/data-table/data-table.component.ts
+++ src/app/data-table/data-table.component.ts
@@ -1,6 +1,7 @@
 /**Importing required files */
 import { Component, OnInit } from '@angular/core';
 import  { GetDataService } from '../get-data.service';
+
 /**Creating meta Data  */
 @Component({
   selector: 'app-data-table',
@@ -10,7 +11,7 @@ import  { GetDataService } from '../get-data.service';
 /**Class to get the data through http request */
 export class DataTableComponent implements OnInit {
  jsonData:string[];
-
+  
   constructor(private api:GetDataService) { } /**Creating service api */
  
  /**Getting data from json data */
@@ -19,12 +20,14 @@ export class DataTableComponent implements OnInit {
      this.api.getData()
      .subscribe(data=>{
        this.jsonData=data.data;
-       console.log(this.jsonData);
+       console.log(this.jsonData)
      })
    }catch(ex){
      console.log(ex);
    }
  }
+
+ 
  /**Declaration of method  */
   ngOnInit() {
     this.getJsonData();
